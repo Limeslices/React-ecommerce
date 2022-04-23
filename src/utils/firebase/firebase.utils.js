4b74680f01628b12040f431e40e34d499b701a90
Kohl <kohlbyrd@MacBook-Air-3.lan>
2022-04-23 00:21:34 -0500

    first commit

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,55 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth'
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDwG8uaH9LfjgUl6uuPu03wmhhCxadAsFU",
+    authDomain: "crwn-clothing-db-f8a4d.firebaseapp.com",
+    projectId: "crwn-clothing-db-f8a4d",
+    storageBucket: "crwn-clothing-db-f8a4d.appspot.com",
+    messagingSenderId: "802048539738",
+    appId: "1:802048539738:web:ae08cf6c8acc9d01b22c63"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider()
+googleProvider.setCustomParameters({
+    prompt: "select_account"
+})
+
+export const auth = getAuth()
+export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider)
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
+
+export const db = getFirestore()
+
+export const createUserDocumentFromAuth = async(userAuth, addtionalInformation = {}) => {
+    if (!userAuth) return;
+
+    const userDocRef = doc(db, 'users', userAuth.uid)
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth
+        const createdAt = new Date()
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt,
+                ...addtionalInformation
+            })
+        } catch (e) {
+            console.log('Error creating the user', e.message);
+        }
+    }
+    return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async(email, password) => {
+    if (!email || !password) return;
+
+    return await createUserWithEmailAndPassword(auth, email, password)
+}
